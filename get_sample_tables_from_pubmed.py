d49ebf49df19198235d0d2d869929512a3affaa8
LedZeppe1in <tualatin32@mail.ru>
2024-03-15 09:11:34 +0800

    Updated get_sample_tables_from_pubmed.py

--- get_sample_tables_from_pubmed.py
+++ get_sample_tables_from_pubmed.py
@@ -1,100 +1,84 @@
-# This param can sufficiently affect a number of tables scrapped from the source
-max_of_articles = 100
+import json
+import pathlib
+
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+max_articles = 100  # this param can sufficiently affect a number of tables scrapped from the source
 
 
 def search_articles(query) -> []:
-    import requests
-    from bs4 import BeautifulSoup as bs
-
     session = requests.Session()
     url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
-
     params = {
         "db": "pmc",
-        "retmax": max_of_articles,
+        "retmax": max_articles,
         "term": query
     }
-
     response = session.get(url=url, params=params)
-    # print(response.text)
-
-    soup = bs(response.text, "lxml")
+    soup = BeautifulSoup(response.text, "lxml")
     tags = soup.findAll("id")
     result = []
     for tag in tags:
         result.append(tag.text)
-
-    # print(result)
     return result
 
 
 def get_article(pmcid) -> str:
-    import requests
-
     session = requests.Session()
     url = "https://www.ncbi.nlm.nih.gov/pmc/oai/oai.cgi"
-
     params = {
         "verb": "GetRecord",
         "identifier": "oai:pubmedcentral.nih.gov:" + pmcid,
         "metadataPrefix": "pmc"
     }
-
     response = session.get(url=url, params=params)
     return response.text
 
 
 def parse_html_table(table):
-    import pandas as pd
-
     n_columns = 0
     n_rows = 0
     column_names = []
-    try:
-        # Find number of rows and columns
-        # Find column titles as possible
-        for row in table.find_all('tr'):
+    # Find number of rows and columns
+    # Find column titles as possible
+    for row in table.find_all('tr'):
 
-            # Determine the number of rows in the table
-            td_nodes = row.find_all('td')
-            if len(td_nodes) > 0:
-                n_rows += 1
-                if n_columns == 0:
-                    # Set the number of columns for our table
-                    n_columns = len(td_nodes)
+        # Determine the number of rows in the table
+        td_nodes = row.find_all('td')
+        if len(td_nodes) > 0:
+            n_rows += 1
+            if n_columns == 0:
+                # Set the number of columns for our table
+                n_columns = len(td_nodes)
 
-            # Handle column names as possible
-            th_nodes = row.find_all('th')
-            if len(th_nodes) > 0 and len(column_names) == 0:
-                for th in th_nodes:
-                    content = th.get_text().replace("\n", " ").strip()
-                    column_names.append(content)
+        # Handle column names as possible
+        th_nodes = row.find_all('th')
+        if len(th_nodes) > 0 and len(column_names) == 0:
+            for th in th_nodes:
+                content = th.get_text().replace("\n", " ").strip()
+                column_names.append(content)
 
-        # Safeguard on the column titles
-        if len(column_names) > 0 and len(column_names) != n_columns:
-            # raise Exception("Column titles do not match the number of columns")
-            return
+    # Safeguard on the column titles
+    if len(column_names) > 0 and len(column_names) != n_columns:
+        # raise Exception("Column titles do not match the number of columns")
+        return
 
-        columns = column_names if len(column_names) > 0 else range(0, n_columns)
-        df = pd.DataFrame(columns=columns, index=range(0, n_rows))
-        row_marker = 0
-        for row in table.find_all('tr'):
-            column_marker = 0
-            columns = row.find_all('td')
-            for column in columns:
-                content = column.get_text().replace("\n", " ").strip()
-                df.iat[row_marker, column_marker] = content
-                column_marker += 1
-            if len(columns) > 0:
-                row_marker += 1
+    columns = column_names if len(column_names) > 0 else range(0, n_columns)
+    df = pd.DataFrame(columns=columns, index=range(0, n_rows))
+    row_marker = 0
+    for row in table.find_all('tr'):
+        column_marker = 0
+        columns = row.find_all('td')
+        for column in columns:
+            content = column.get_text().replace("\n", " ").strip()
+            df.iat[row_marker, column_marker] = content
+            column_marker += 1
+        if len(columns) > 0:
+            row_marker += 1
 
-        return df
-    except:
-        pass
-
-
-def convert_table_from_html_to_json(table):
-    print("get table here")
+    return df
 
 
 def save_tables(directory_for_tables, topic):
@@ -104,10 +88,6 @@ def save_tables(directory_for_tables, topic):
     :param topic: название темы для поиска статей
     :return:
     """
-    import json
-    import pathlib
-    from bs4 import BeautifulSoup as bs
-
     pcmids = search_articles(topic)
     print(pcmids)
 
@@ -115,7 +95,7 @@ def save_tables(directory_for_tables, topic):
         for pcmid in pcmids:
             text = get_article(pcmid)
             text = text.encode("latin-1")
-            bs_content = bs(text, "lxml")
+            bs_content = BeautifulSoup(text, "lxml")
             table_nodes = bs_content.findAll("table")
             if len(table_nodes) > 0:
                 print(len(table_nodes), "tables detected in", pcmid)
